6a381050eeb855daefbe3692233926f87b81d8e4
maverik-io <josephchacko2006@gmail.com>
2024-07-29 13:52:55 +0530

    added colors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use colored::*;
 use std::env;
 use std::fs;
 
@@ -12,67 +13,72 @@ fn main() {
 
 fn print_dir_contents(dir: &std::path::Path, indent: u32, args: &Vec<String>) {
     for file in fs::read_dir(dir).unwrap() {
-        let mut icon = String::from("    ");
+        let mut icon = String::from("    ").white();
 
         let file = file.unwrap();
 
         match file.file_name().into_string().unwrap().split(".").last() {
-            Some("rs") => icon = String::from("  󱘗  "),
-            Some("md") => icon = String::from("    "),
-            Some("sh") => icon = String::from("    "),
-            Some("json") => icon = String::from("    "),
-            Some("yaml") => icon = String::from("    "),
-            Some("toml") => icon = String::from("    "),
-            Some("yml") => icon = String::from("    "),
-            Some("lock") => icon = String::from("    "),
-            Some("py") => icon = String::from("  󰌠  "),
-            Some("pyc") => icon = String::from("  󰌠  "),
-            Some("html") => icon = String::from("    "),
-            Some("css") => icon = String::from("    "),
-            Some("js") => icon = String::from("    "),
-            Some("ts") => icon = String::from("    "),
-            Some("jsx") => icon = String::from("    "),
-            Some("tsx") => icon = String::from("    "),
-            Some("java") => icon = String::from("    "),
-            Some("c") => icon = String::from("    "),
-            Some("cpp") => icon = String::from("    "),
-            Some("h") => icon = String::from("    "),
-            Some("hpp") => icon = String::from("    "),
-            Some("go") => icon = String::from("  󰟓  "),
-            Some("rb") => icon = String::from("    "),
-            Some("php") => icon = String::from("    "),
-            Some("lua") => icon = String::from("    "),
-            Some("sql") => icon = String::from("    "),
-            Some("swift") => icon = String::from("    "),
-            Some("psd") => icon = String::from("    "),
-            Some("ai") => icon = String::from("    "),
-            Some("pdf") => icon = String::from("  󰈦  "),
-            Some("png") => icon = String::from("    "),
-            Some("jpg") => icon = String::from("    "),
-            Some("jpeg") => icon = String::from("    "),
-            Some("gif") => icon = String::from("    "),
-            Some("svg") => icon = String::from("    "),
-            Some("webp") => icon = String::from("    "),
-            Some("mp3") => icon = String::from("  󰝚  "),
-            Some("wav") => icon = String::from("  󰝚  "),
-            Some("flac") => icon = String::from("  󰝚  "),
-            Some("ogg") => icon = String::from("  󰝚  "),
-            Some("m4a") => icon = String::from("  󰝚  "),
-            Some("opus") => icon = String::from("  󰝚  "),
+            Some("rs") => icon = String::from("  󱘗  ").white(),
+            Some("md") => icon = String::from("    ").white(),
+            Some("sh") => icon = String::from("    ").white(),
+            Some("json") => icon = String::from("    ").white(),
+            Some("yaml") => icon = String::from("    ").white(),
+            Some("toml") => icon = String::from("    ").white(),
+            Some("yml") => icon = String::from("    ").white(),
+            Some("lock") => icon = String::from("    ").white(),
+            Some("py") => icon = String::from("  󰌠  ").white(),
+            Some("pyc") => icon = String::from("  󰌠  ").white(),
+            Some("html") => icon = String::from("    ").white(),
+            Some("css") => icon = String::from("    ").white(),
+            Some("js") => icon = String::from("    ").white(),
+            Some("ts") => icon = String::from("    ").white(),
+            Some("jsx") => icon = String::from("    ").white(),
+            Some("tsx") => icon = String::from("    ").white(),
+            Some("java") => icon = String::from("    ").white(),
+            Some("c") => icon = String::from("    ").white(),
+            Some("cpp") => icon = String::from("    ").white(),
+            Some("h") => icon = String::from("    ").white(),
+            Some("hpp") => icon = String::from("    ").white(),
+            Some("go") => icon = String::from("  󰟓  ").white(),
+            Some("rb") => icon = String::from("    ").white(),
+            Some("php") => icon = String::from("    ").white(),
+            Some("lua") => icon = String::from("    ").white(),
+            Some("sql") => icon = String::from("    ").white(),
+            Some("swift") => icon = String::from("    ").white(),
+            Some("psd") => icon = String::from("    ").white(),
+            Some("ai") => icon = String::from("    ").white(),
+            Some("pdf") => icon = String::from("  󰈦  ").white(),
+            Some("png") => icon = String::from("    ").white(),
+            Some("jpg") => icon = String::from("    ").white(),
+            Some("jpeg") => icon = String::from("    ").white(),
+            Some("gif") => icon = String::from("    ").white(),
+            Some("svg") => icon = String::from("    ").white(),
+            Some("webp") => icon = String::from("    ").white(),
+            Some("mp3") => icon = String::from("  󰝚  ").white(),
+            Some("wav") => icon = String::from("  󰝚  ").white(),
+            Some("flac") => icon = String::from("  󰝚  ").white(),
+            Some("ogg") => icon = String::from("  󰝚  ").white(),
+            Some("m4a") => icon = String::from("  󰝚  ").white(),
+            Some("opus") => icon = String::from("  󰝚  ").white(),
 
             Some(&_) => (),
             None => (),
         }
 
+        let out = file.file_name().into_string().unwrap();
+        let mut out = out.split("/").last().unwrap().to_string();
+
+        let indent_str = " ▏".repeat(indent as usize);
+
         if file.file_type().unwrap().is_dir() {
-            icon = String::from("   ")
+            icon = String::from("   ").bold().yellow();
+            out = format!("{}{}{}", indent_str.black(), icon.bold(), out.bold());
+        } else {
+            out = format!("{}{}{}", indent_str.black(), icon, out);
         }
 
-        let out = file.file_name().into_string().unwrap();
-        let out = out.split("/").last().unwrap();
-        let indent_str = " ▏".repeat(indent as usize);
         if !out.starts_with(".") || args.contains(&String::from("-a")) {
-            println!("{}{}{}", indent_str, icon, out);
+            println!("{}", out);
 
             if file.file_type().unwrap().is_dir() {
                 let next_indent = indent + 1;
